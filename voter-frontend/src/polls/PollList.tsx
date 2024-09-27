7b9b1353fc97492cd8236a4483dc149604a447bc
Roland Thompson <rolanditaru@gmail.com>
2024-09-28 07:57:14 +1000

    Remove old frontend code

--- voter-frontend/src/polls/PollList.tsx
+++
@@ -1,9 +0,0 @@
-export default function PollList({ data }: { data: string[] }) {
-  return (
-    <ul>
-      {data.map((poll) => (
-        <li key={poll}>{poll}</li>
-      ))}
-    </ul>
-  );
-}
